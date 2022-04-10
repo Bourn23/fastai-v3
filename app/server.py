0b06909f0416563faf8977efccff32eadb9b2ac0
bourn23 <bourn23@gmail.com>
2022-04-10 11:31:55 -0400

    fix server port

--- app/server.py
+++ app/server.py
@@ -77,4 +77,4 @@ async def predict(request):
 
 if __name__ == '__main__':
     if 'serve' in sys.argv:
-        uvicorn.run(app=app, host='0.0.0.0', port=3001, log_level="info")
+        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
